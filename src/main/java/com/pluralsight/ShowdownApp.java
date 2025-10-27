cf96068ae1c5f0ffe891cae22644b38592eec551
Eric Schwartz <erics273@gmail.com>
2025-10-27 15:02:27 -0400

    Super Fighting is happening

--- src/main/java/com/pluralsight/ShowdownApp.java
+++ src/main/java/com/pluralsight/ShowdownApp.java
@@ -39,6 +39,7 @@ public class ShowdownApp {
         //create a SuperHero form our class SuperHero that extends SuperPerson
         SuperHero myHero = new SuperHero("EricTheHero", 100, 25);
 
+
         myHero.fight(jazzyLi);
 
         //see what type of class eric is
@@ -46,6 +47,21 @@ public class ShowdownApp {
 
         //see what type of class eric is
         System.out.println("EricTheHero is an instance of: " + myHero.getClass());
+
+        //test the fighting stuff
+        SuperHero hero1 = new SuperHero("SuperDude!", 100, 15);
+        SuperHero hero2 = new SuperHero("SuperGirl!", 115, 25);
+
+        //print some stats
+        System.out.println("Before fighting " + hero1.getName() + " has " + hero1.getHealth() + " health");
+        System.out.println("Before fighting " + hero2.getName() + " has " + hero2.getHealth() + " health");
+
+        hero1.fight(hero2);
+        hero2.fight(hero1);
+
+        //print some stats
+        System.out.println("After fighting " + hero1.getName() + " has " + hero1.getHealth() + " health");
+        System.out.println("After fighting " + hero2.getName() + " has " + hero2.getHealth() + " health");
 
     }
 
