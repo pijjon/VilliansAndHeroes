b90dbe46d31298fd6327c0e85363829d34f25fcd
Alwin Zhao <alwinzhao12@gmail.com>
2025-10-28 11:59:33 -0400

    add duel() static method to ShowdownApp which will facilitate battle until one participant loses all health

--- src/main/java/com/pluralsight/ShowdownApp.java
+++ src/main/java/com/pluralsight/ShowdownApp.java
@@ -4,65 +4,45 @@ public class ShowdownApp {
 
     public static void main(String[] args) {
 
-        //create 2 super people
-        SuperPerson eric =  new SuperPerson("Eric", 100);
-        SuperPerson jazzyLi  = new SuperPerson("JazzyLi", 85);
+        // create 2 super people
+        SuperPerson hero = new SuperHero("Deku", 100, 56);
+        SuperPerson villain = new SuperVillain("Dabi", 100, 60);
 
-        //print out the status of each SuperPerson
-        System.out.println(eric.getStatus());
-        System.out.println(jazzyLi.getStatus());
-
-        //deal some damage
-        eric.takeDamage(15);
-        jazzyLi.takeDamage(5);
-
-        //print out the status of each SuperPerson
-        System.out.println(eric.getStatus());
-        System.out.println(jazzyLi.getStatus());
-
-        if(eric.isAlive()){
-            System.out.println("Eric Lives!");
-        }else{
-            System.out.println("Eric is dead as hell");
-        }
-
-        if(jazzyLi.isAlive()){
-            System.out.println("JazzyLi Lives!");
-        }else{
-            System.out.println("JazzyLi is dead as hell");
-        }
-
-        //make these SuperPeople fight each other
-        eric.fight(jazzyLi);
-        jazzyLi.fight(eric);
-
-        //create a SuperHero form our class SuperHero that extends SuperPerson
-        SuperHero myHero = new SuperHero("EricTheHero", 100, 25);
-
-
-        myHero.fight(jazzyLi);
-
-        //see what type of class eric is
-        System.out.println("Eric is an instance of: " + eric.getClass());
-
-        //see what type of class eric is
-        System.out.println("EricTheHero is an instance of: " + myHero.getClass());
-
-        //test the fighting stuff
-        SuperHero hero1 = new SuperHero("SuperDude!", 100, 15);
-        SuperHero hero2 = new SuperHero("SuperGirl!", 115, 25);
-
-        //print some stats
-        System.out.println("Before fighting " + hero1.getName() + " has " + hero1.getHealth() + " health");
-        System.out.println("Before fighting " + hero2.getName() + " has " + hero2.getHealth() + " health");
-
-        hero1.fight(hero2);
-        hero2.fight(hero1);
-
-        //print some stats
-        System.out.println("After fighting " + hero1.getName() + " has " + hero1.getHealth() + " health");
-        System.out.println("After fighting " + hero2.getName() + " has " + hero2.getHealth() + " health");
+        duel(hero, villain);
 
     }
+
+
+    public static void duel(SuperPerson fighter1, SuperPerson fighter2) {
+        SuperPerson attacker;
+        SuperPerson defender;
+        if (fighter1.getHealth() == 0 || fighter2.getHealth() == 0) {
+            System.out.println("Unable to duel. One or more fighters already down.");
+            return;
+        } else if (fighter1.getExperiencePoints() > fighter2.getHealth()) {
+            attacker = fighter1;
+            defender = fighter2;
+            System.out.println(fighter1 + " makes the first move!");
+        } else {
+            attacker = fighter2;
+            defender = fighter1;
+            System.out.println(fighter2 + " makes the first move!");
+        }
+
+        for (int i = 1; fighter1.getHealth() > 0 && fighter2.getHealth() > 0; i++) {
+            System.out.println("Turn " + i + ":");
+            attacker.attack(defender);
+            if (defender.isAlive()) {
+                SuperPerson temp = attacker;
+                attacker = defender;
+                defender = temp;
+            }
+            else {
+                System.out.println(attacker.getName() + " has defeated " + defender.getName());
+            }
+        }
+
+    }
+
 
 }
