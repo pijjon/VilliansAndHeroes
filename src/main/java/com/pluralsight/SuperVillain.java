ddf79171cc107cbdf1a71fec5eff8a081fbe3188
Alwin Zhao <alwinzhao12@gmail.com>
2025-10-28 10:33:14 -0400

    update SuperVillain class that extends SuperPerson

--- src/main/java/com/pluralsight/SuperVillain.java
+++ src/main/java/com/pluralsight/SuperVillain.java
@@ -1,5 +1,7 @@
 package com.pluralsight;
 
+import java.util.Random;
+
 public class SuperVillain extends SuperPerson   {
 
     public SuperVillain(String name, int health, int evilPoints) {
@@ -8,5 +10,28 @@ public class SuperVillain extends SuperPerson   {
         this.experiencePoints = evilPoints;
     }
 
+    @Override
+    public void fight(SuperPerson opponent){
 
+        //generate a random number of damage to do
+        Random rand = new Random();
+        //return a whole number between 0 and 101 inculusive of the 0 but not the 101 (0-100)
+        int baseDamage = rand.nextInt(101);
+
+        //lets make the total damage interesting by taking into account our experiencePoints
+        int totalDamage = baseDamage + this.experiencePoints;
+
+        //determine if we missed or hit based on the baseDamage
+        //if we got a 0 for the base damage we missed
+        //otherwise we landed our attack on our opponent and took the damage away from their health
+        if(baseDamage == 0){
+            System.out.println(this.getName() + "  Swings heroically and misses because that happens sometimes");
+        }else{
+            System.out.println(this.getName() + " lands a super punch on  " + opponent.getName() + " and caused " + totalDamage + " damage");
+
+            //actually cause the damage to the opponent
+            opponent.takeDamage(totalDamage);
+        }
+
+    }
 }
